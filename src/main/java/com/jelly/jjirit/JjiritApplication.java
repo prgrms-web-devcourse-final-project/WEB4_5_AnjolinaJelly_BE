56c766ca51fd4cd5871ad5e667c03effe2e6fc1d
Woongbin06 <kob05314@gmail.com>
2025-04-27 21:14:28 +0900

    Feat : 프로젝트 생성

---
+++ src/main/java/com/jelly/jjirit/JjiritApplication.java
@@ -0,0 +1,13 @@
+package com.jelly.jjirit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjiritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjiritApplication.class, args);
+	}
+
+}
