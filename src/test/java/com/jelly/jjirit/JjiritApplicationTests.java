4b3beeb553a04dc020ea6d44b252ffb4dd919ce9
강웅빈 <2022049@bssm.hs.kr>
2025-04-28 00:20:21 +0900

    Feat: 프로젝트 생성 및 세팅 (#2)
    * Feat: 프로젝트 생성
    
    * Feat: application.yml 세팅

---
+++ src/test/java/com/jelly/jjirit/JjiritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelly.jjirit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjiritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
