c8f8ced369e4fa1896fd6fbba6423c1f6994c4b9
Sojinyoung <wo0982@naver.com>
2025-04-29 14:34:23 +0900

    [feat] 이메일 프로세스 작업 및 로그인, 회원가입 로직

---
+++ src/main/java/com/jelly/zzirit/domain/member/dto/req/EmailAuthVerificationDTO.java
@@ -0,0 +1,19 @@
+package com.jelly.zzirit.domain.member.dto.req;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailAuthVerificationDTO {
+	@NotNull(message = "이메일은 필수입니다.")
+	@Email(message = "유효한 이메일을 입력해주세요.")
+	private String email;
+
+	@NotNull(message = "인증 코드는 필수입니다.")
+	private String code;
+}
