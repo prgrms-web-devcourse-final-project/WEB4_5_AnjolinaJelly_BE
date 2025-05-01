be5a232d7c31abb532425b5112f0d8d77cd8149d
Sojinyoung <wo0982@naver.com>
2025-05-01 15:24:18 +0900

    temp

--- src/main/java/com/jelly/zzirit/domain/member/controller/InfoController.java
+++ src/main/java/com/jelly/zzirit/domain/member/controller/InfoController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jelly.zzirit.global.AuthMember;
-import com.jelly.zzirit.global.dto.BaseResponse;
-import com.jelly.zzirit.global.dto.Empty;
 import com.jelly.zzirit.global.security.oauth2.service.token.OAuthTempTokenService;
 
 import jakarta.servlet.http.HttpServletRequest;
