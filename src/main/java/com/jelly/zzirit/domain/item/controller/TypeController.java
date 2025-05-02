4759bf543d4b7aa8f9dabbfa04ca19ccdf2faed8
Woongbin06 <kob05314@gmail.com>
2025-05-02 17:24:02 +0900

    Feat : 상품 종류 Tag 추가

--- src/main/java/com/jelly/zzirit/domain/item/controller/TypeController.java
+++ src/main/java/com/jelly/zzirit/domain/item/controller/TypeController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jelly.zzirit.domain.item.dto.TypeResponse;
 import com.jelly.zzirit.global.dto.BaseResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/types")
+@Tag(name = "상품 종류 API", description = "상품 종류와 관련된 API를 설명합니다.")
 public class TypeController {
 
 	@GetMapping
+	@Operation(summary = "상품 종류 전체 조회", description = "상품 종류를 전체 조회 합니다.")
 	public BaseResponse<List<TypeResponse>> findType() {
 		return BaseResponse.success(
 			List.of(new TypeResponse(1L, "노트북"))
