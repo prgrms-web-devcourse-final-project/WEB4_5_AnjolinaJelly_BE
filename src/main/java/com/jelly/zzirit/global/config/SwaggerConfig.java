c5d6d6afb7597cd6a687699a83ecb723b4809e83
Hanjise0ng <tjdwl3269@gmail.com>
2025-05-02 15:12:06 +0900

    Chore: Swagger 설정 추가

---
+++ src/main/java/com/jelly/zzirit/global/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.jelly.zzirit.global.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("bearer", this.securityScheme()))
+			.security(Collections.singletonList(this.securityRequirement()))
+			.info(
+				new Info().title("zzirit API")
+					.description("찌릿 API 명세서")
+					.version("v0.0.1")
+			);
+	}
+
+	private SecurityScheme securityScheme() {
+		return new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER).name("Authorization");
+	}
+
+	private SecurityRequirement securityRequirement() {
+		return new SecurityRequirement().addList("bearer");
+	}
+}
