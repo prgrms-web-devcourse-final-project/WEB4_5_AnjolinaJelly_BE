2078c149ec20aebb9d6ec776edb476faeb4fde07
Jisu Kim <jisukim.tech@gmail.com>
2025-05-04 00:45:33 +0900

    Style: 코드 들여쓰기 맞춤

--- src/main/java/com/jelly/zzirit/domain/order/dto/response/OrderFetchResponse.java
+++ src/main/java/com/jelly/zzirit/domain/order/dto/response/OrderFetchResponse.java
@@ -9,12 +9,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record OrderFetchResponse(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
     LocalDateTime orderDate, // 주문 날짜
-        Long orderId, // 주문 아이디 // 주문 취소 시 필요
-        String orderNumber, // 주문 번호
-        BigDecimal totalPrice, // 총 주문 금액 // (개당 가격 * 개수)의 합
-        Order.OrderStatus orderStatus, // 주문 상태
-        List<OrderItemFetchResponse> items // 해당 주문에 포함된 상품 데이터
+    Long orderId, // 주문 아이디 // 주문 취소 시 필요
+    String orderNumber, // 주문 번호
+    BigDecimal totalPrice, // 총 주문 금액 // (개당 가격 * 개수)의 합
+    Order.OrderStatus orderStatus, // 주문 상태
+    List<OrderItemFetchResponse> items // 해당 주문에 포함된 상품 데이터
 ) {
 }
