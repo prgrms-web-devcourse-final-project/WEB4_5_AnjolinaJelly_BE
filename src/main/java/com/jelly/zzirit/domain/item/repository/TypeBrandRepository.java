a10754bc57d873e426efd16c266377d5a55f011b
Woongbin06 <kob05314@gmail.com>
2025-05-04 16:36:54 +0900

    Feat : 상품 브랜드 조회

---
+++ src/main/java/com/jelly/zzirit/domain/item/repository/TypeBrandRepository.java
@@ -0,0 +1,12 @@
+package com.jelly.zzirit.domain.item.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jelly.zzirit.domain.item.entity.TypeBrand;
+
+public interface TypeBrandRepository extends JpaRepository<TypeBrand, Long> {
+
+	List<TypeBrand> findByTypeId(Long typeId);
+}
