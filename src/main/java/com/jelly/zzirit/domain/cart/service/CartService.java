248ea4ec308a65e28e94078f943f3c14c6a81a2c
Hanjise0ng <tjdwl3269@gmail.com>
2025-05-05 21:08:08 +0900

    refactor: 장바구니 조회 가격 계산 단순화

--- src/main/java/com/jelly/zzirit/domain/cart/service/CartService.java
+++ src/main/java/com/jelly/zzirit/domain/cart/service/CartService.java
@@ -1,6 +1,5 @@
 package com.jelly.zzirit.domain.cart.service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,7 +13,6 @@ import com.jelly.zzirit.domain.cart.repository.CartRepository;
 import com.jelly.zzirit.domain.item.entity.Item;
 import com.jelly.zzirit.domain.item.entity.ItemStatus;
 import com.jelly.zzirit.domain.item.entity.timedeal.TimeDealItem;
-import com.jelly.zzirit.domain.item.repository.ItemRepository;
 import com.jelly.zzirit.domain.item.repository.TimeDealItemRepository;
 import com.jelly.zzirit.global.dto.BaseResponseStatus;
 import com.jelly.zzirit.global.exception.custom.InvalidUserException;
@@ -27,7 +25,6 @@ public class CartService {
 
 	private final CartRepository cartRepository;
 	private final CartItemRepository cartItemRepository;
-	private final ItemRepository itemRepository;
 	private final TimeDealItemRepository timeDealItemRepository;
 
 	public CartResponse getMyCart(Long memberId) {
@@ -44,29 +41,23 @@ public class CartService {
 			.map(cartItem -> {
 				Item item = cartItem.getItem();
 				int quantity = cartItem.getQuantity();
-				BigDecimal originPrice = item.getPrice();
-				int unitPrice = originPrice.intValue();
 
-				// 타임딜 적용 여부
 				boolean isTimeDeal = item.getItemStatus() == ItemStatus.TIME_DEAL;
 				Integer discountRatio = null;
-				int discountedPrice = unitPrice;
 
-				if (isTimeDeal) {
-					// 타임딜 정보 조회
-					TimeDealItem timeDealItem = timeDealItemRepository
-						.findActiveTimeDealItemByItemId(item.getId())
-						.orElse(null);
+				TimeDealItem timeDealItem = isTimeDeal
+					? timeDealItemRepository.findActiveTimeDealItemByItemId(item.getId()).orElse(null)
+					: null;
 
-					if (timeDealItem != null) {
-						BigDecimal dealPrice = timeDealItem.getPrice();
-						discountedPrice = dealPrice.intValue();
+				int unitPrice = (timeDealItem != null)
+					? timeDealItem.getPrice().intValue()
+					: item.getPrice().intValue();
 
-						discountRatio = timeDealItem.getTimeDeal().getDiscountRatio();
-					}
-				}
+				discountRatio = (timeDealItem != null)
+					? timeDealItem.getTimeDeal().getDiscountRatio()
+					: null;
 
-				int totalPrice = discountedPrice * quantity;
+				int totalPrice = unitPrice * quantity;
 
 				return new CartItemResponse(
 					cartItem.getId(),
@@ -75,11 +66,9 @@ public class CartService {
 					item.getImageUrl(),
 					quantity,
 					unitPrice,
-					// , discountedUnitPrice // 추후 FE 협의 후 포함 가능
 					totalPrice,
 					isTimeDeal,
 					discountRatio
-
 				);
 			})
 			.toList();
