7dab170a1c375f4c7d221ee21a5839b8ca115426
Huipadyam <160405935+huipadyam@users.noreply.github.com>
2025-05-05 12:17:00 +0900

    Feat: 관리자 상품 목록 조회 api 구현 (#56)
    * Feat: 관리자 상품 목록 조회 api ã구현
    
    * Refactor: BaseResponse 감싸는 코드를 서비스 -> ì»ì스에서¬로 옮김
    
    * Feat: 관리자 상품 접근 제한
    
    * Feat: 관리자 계정 테스트용으로 자동 생성
    
    * Refactor: 패초기화 패키지로 이동, 중복 생성 방지

---
+++ src/main/java/com/jelly/zzirit/domain/adminItem/service/QueryAdminItemService.java
@@ -0,0 +1,39 @@
+package com.jelly.zzirit.domain.adminItem.service;
+
+import com.jelly.zzirit.domain.adminItem.dto.response.AdminItemResponse;
+import com.jelly.zzirit.domain.item.entity.Item;
+import com.jelly.zzirit.domain.item.entity.stock.ItemStock;
+import com.jelly.zzirit.domain.order.repository.ItemStockRepository;
+import com.jelly.zzirit.global.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.jelly.zzirit.domain.item.repository.ItemRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAdminItemService {
+    private final ItemRepository itemRepository;
+    private final ItemStockRepository itemStockRepository;
+
+    public List<AdminItemResponse> getItems() {
+        List<Item> items = itemRepository.findAll();
+
+        // N+1문제 막기 위해 미리 찾아서 map 만들어둠
+        // Todo: item과 item stock의 id를 같게 관리하면 어떨까?
+        List<Long> itemIds = items.stream().map(Item::getId).toList(); // 상품 id 목록
+        List<ItemStock> itemStocks = itemStockRepository.findAllByItemIdIn(itemIds); // 재고 개수 목록
+        Map<Long, ItemStock> itemStockMap = itemStocks.stream().collect(Collectors.toMap(ItemStock::getId, Function.identity()));
+        // Map<재고 id, 재고 객체>
+
+
+        return items.stream()
+                        // from(Item, ItemStock)
+                        .map(item -> AdminItemResponse.from(item, itemStockMap.get(item.getId())))
+                        .toList();
+    }
+}
