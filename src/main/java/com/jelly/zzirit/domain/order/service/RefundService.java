a9603b170e694d0739deeecc5c48549b19cff4ea
jisukimtech <jisukim.tech@gmail.com>
2025-05-07 05:04:48 +0900

    Feat: 외부 API를 이용한 결제 취소 기능 구현

--- src/main/java/com/jelly/zzirit/domain/order/service/RefundService.java
+++ src/main/java/com/jelly/zzirit/domain/order/service/RefundService.java
@@ -1,6 +1,8 @@
 package com.jelly.zzirit.domain.order.service;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,4 +56,55 @@ public class RefundService {
 			throw new IllegalStateException("환불 처리에 실패했습니다. 관리자에게 문의하세요.");
 		}
 	}
+
+	/**
+	 * 주문 취소 요청에 따른 결제 취소
+	 * @param orderId 취소할 주문의 아이디
+	 * @param paymentKey 결제 정보 키
+	 */
+	public void refund(Long orderId, String paymentKey) {
+		String url = "https://api.tosspayments.com/v1/payments/" + paymentKey + "/cancel";
+
+		HttpHeaders headers = getHeaders(); // 헤더 생성
+		Map<String, Object> body = getBody(); // 바디 생성
+
+		HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+		try {
+			ResponseEntity<String> response = restTemplate.exchange(
+				url,
+				HttpMethod.POST,
+				request,
+				String.class
+			);
+
+			if (!response.getStatusCode().is2xxSuccessful()) {
+				log.error("환불 실패: status={}, body={}", response.getStatusCode(), response.getBody());
+				throw new IllegalStateException("토스 결제 취소 API 실패");
+			}
+		} catch (Exception e) {
+			log.error("환불 실패: orderId={}, message={}", orderId, e.getMessage(), e);
+			throw new IllegalStateException("환불 처리 실패");
+		}
+	}
+
+	private HttpHeaders getHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		String encodedKey = Base64.getEncoder().encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("Authorization", "Basic " + encodedKey);
+
+		return headers;
+	}
+
+	private Map<String, Object> getBody() {
+		String cancelReason = "구매자 변심으로 인한 주문 취소";
+		Map<String, Object> body = new HashMap<>();
+
+		body.put("cancelReason", cancelReason);
+
+		return body;
+	}
+
 }
