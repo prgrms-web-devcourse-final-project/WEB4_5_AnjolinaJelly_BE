6a069dcafb239614d8a70d99f04f2c82fe37f66e
huipadyam <kces0822@gmail.com>
2025-05-07 17:54:25 +0900

    Feat: ì관리자 상품 조ì회 시 초기 재고와 판매된 재고 모두 조회

--- src/main/java/com/jelly/zzirit/domain/adminItem/dto/response/AdminItemResponse.java
+++ src/main/java/com/jelly/zzirit/domain/adminItem/dto/response/AdminItemResponse.java
@@ -12,7 +12,8 @@ public record AdminItemResponse (
     String type,
     String brand,
     int price,
-    int stockQuantity
+    int initialStockQuantity,
+    int soldStockQuantity
 ){
     public static AdminItemResponse from (Item item, ItemStock itemStock) { // item에 해당하는 itemStock을 조회해서 넣어야함(단방향이라서)
         return AdminItemResponse.builder()
@@ -22,7 +23,8 @@ public record AdminItemResponse (
                 .type(item.getTypeBrand().getType().getName()) // TODO: enum으로 관리?
                 .brand(item.getTypeBrand().getBrand().getName())
                 .price(item.getPrice().intValue()) // TODO: 추후 bigdecimal로 스펙 변경 필요
-                .stockQuantity(itemStock.getQuantity() - itemStock.getSoldQuantity()) // Todo: 재고수량 둘 다 줄지? 상품-재고 분리?(재고는 캐시x?), 단방향(양방향 할거면 합치기?)
+                .initialStockQuantity(itemStock.getQuantity()) // Todo: 재고수량 둘 다 줄지? 상품-재고 분리?(재고는 캐시x?), 단방향(양방향 할거면 합치기?)
+                .soldStockQuantity(itemStock.getSoldQuantity())
                 .build();
     }
 }
