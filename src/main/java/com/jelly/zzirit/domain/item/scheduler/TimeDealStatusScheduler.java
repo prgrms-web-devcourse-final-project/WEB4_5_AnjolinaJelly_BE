73dd2519ca66a06b25c42484c67b48fd11f831b4
cloudmato <cloudmato00@gmail.com>
2025-05-09 11:29:39 +0900

    Refactor: 타임딜 상태 변경 스케쥴러 리팩토링 및 트랜잭션 추가 - 스케쥴러 내 상태 변경 로직 서비스로 이동 - 상태 변경 로직 서비스 코드 분리(예정 -> 진행중, 진행중 -> 종료) - 개별 서비스 메서드 트랜잭션 추가

--- src/main/java/com/jelly/zzirit/domain/item/scheduler/TimeDealStatusScheduler.java
+++ src/main/java/com/jelly/zzirit/domain/item/scheduler/TimeDealStatusScheduler.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.jelly.zzirit.domain.item.entity.timedeal.TimeDeal;
-import com.jelly.zzirit.domain.item.repository.TimeDealRepository;
+import com.jelly.zzirit.domain.item.service.TimeDealService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,22 +17,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class TimeDealStatusScheduler {
 
-	private final TimeDealRepository timeDealRepository;
+	private final TimeDealService timeDealService;
 
-	@Transactional
 	@Scheduled(fixedRate = 60_000) // 1분마다 실행
 	public void updateTimeDealStatuses() {
 		LocalDateTime now = LocalDateTime.now();
 
-		// 시작 시간이 지났지만 아직 시작되지 않은 타임딜 (SCHEDULED → ONGOING)
-		List<TimeDeal> toStartDeals = timeDealRepository.findAllByStatusAndStartTimeLessThanEqual(
-			TimeDeal.TimeDealStatus.SCHEDULED, now);
-		toStartDeals.forEach(deal -> deal.updateStatus(TimeDeal.TimeDealStatus.ONGOING));
-
-		// 종료 시간이 지난 타임딜 (ONGOING → ENDED)
-		List<TimeDeal> toEndDeals = timeDealRepository.findAllByStatusAndEndTimeBefore(TimeDeal.TimeDealStatus.ONGOING,
-			now);
-		toEndDeals.forEach(deal -> deal.updateStatus(TimeDeal.TimeDealStatus.ENDED));
+		List<TimeDeal> toStartDeals = timeDealService.convertTimeDealStatusScheduledToOngoing(now);
+		List<TimeDeal> toEndDeals = timeDealService.converTimeDealStatusOngoingToEnded(now);
 
 		log.info("시작된 타임딜: {}개, 종료된 타임딜: {}개", toStartDeals.size(), toEndDeals.size());
 	}
