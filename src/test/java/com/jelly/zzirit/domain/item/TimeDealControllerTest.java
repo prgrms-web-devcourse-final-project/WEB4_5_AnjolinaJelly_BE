2045467969c71c37221db4387a2dd43e47a527f3
cloudmato <cloudmato00@gmail.com>
2025-05-09 12:27:15 +0900

    fix: 타임딜 컨트롤러 테스트 오류 해결

--- src/test/java/com/jelly/zzirit/domain/item/TimeDealControllerTest.java
+++ src/test/java/com/jelly/zzirit/domain/item/TimeDealControllerTest.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jelly.zzirit.domain.item.dto.request.TimeDealCreateRequest;
 import com.jelly.zzirit.domain.item.dto.response.timeDeal.TimeDealModalCreateResponse;
-import com.jelly.zzirit.domain.item.repository.ItemStockRepository;
-import com.jelly.zzirit.domain.item.repository.TimeDealItemRepository;
-import com.jelly.zzirit.domain.item.repository.TimeDealRepository;
 import com.jelly.zzirit.domain.member.entity.authenum.Role;
 import com.jelly.zzirit.global.security.util.JwtUtil;
 import com.jelly.zzirit.testutil.TimeDealTestHelper;
@@ -42,22 +38,6 @@ public class TimeDealControllerTest {
 
 	@Autowired
 	TimeDealTestHelper timeDealTestHelper;
-
-	@Autowired
-	private TimeDealItemRepository timeDealItemRepository;
-
-	@Autowired
-	private TimeDealRepository timeDealRepository;
-
-	@Autowired
-	private ItemStockRepository itemStockRepository;
-
-	@BeforeEach
-	void cleanUp() {
-		timeDealItemRepository.deleteAllInBatch();
-		timeDealRepository.deleteAllInBatch();
-		itemStockRepository.deleteAllInBatch();
-	}   // 재고 삭제
 
 	@Test
 	void 타임딜_등록_성공() throws Exception {
@@ -103,7 +83,7 @@ public class TimeDealControllerTest {
 		// 테스트용 타임딜 데이터 생성
 		TimeDealCreateRequest request = new TimeDealCreateRequest(
 			"테스트 타임딜",
-			LocalDateTime.now().plusHours(1),
+			LocalDateTime.now().minusHours(1),
 			LocalDateTime.now().plusHours(2),
 			20,
 			List.of(
@@ -119,9 +99,9 @@ public class TimeDealControllerTest {
 		mockMvc.perform(get("/api/time-deals/now")
 				.cookie(new Cookie("access", accessToken)))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.result.timeDealId").isNumber())
-			.andExpect(jsonPath("$.result.timeDealName").isString())
-			.andExpect(jsonPath("$.result.items").isArray());
+			.andExpect(jsonPath("$.result[0].timeDealName").isString())
+			.andExpect(jsonPath("$.result[0].startTime").isString())
+			.andExpect(jsonPath("$.result[0].items").isArray());
 	}
 
 	@Test
