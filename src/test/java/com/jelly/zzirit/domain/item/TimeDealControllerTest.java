876ffbfe543fd150ffe4831f922e2ac485c03836
cloudmato <cloudmato00@gmail.com>
2025-05-09 16:24:32 +0900

    fix: ActiveProfiles 추가

--- src/test/java/com/jelly/zzirit/domain/item/TimeDealControllerTest.java
+++ src/test/java/com/jelly/zzirit/domain/item/TimeDealControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,6 +27,7 @@ import jakarta.servlet.http.Cookie;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class TimeDealControllerTest extends AcceptanceTest {
 
 	@Autowired
