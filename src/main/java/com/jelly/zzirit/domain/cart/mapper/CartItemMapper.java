3241293914d057a8e53877b0c19459dedbffb5ed
Hanjise0ng <tjdwl3269@gmail.com>
2025-05-13 17:23:16 +0900

    Refactor: mapper 구조 개선

--- src/main/java/com/jelly/zzirit/domain/cart/mapper/CartItemMapper.java
+++ src/main/java/com/jelly/zzirit/domain/cart/mapper/CartItemMapper.java
@@ -9,30 +9,33 @@ import com.jelly.zzirit.domain.item.entity.timedeal.TimeDealItem;
 
 public class CartItemMapper {
 
-	public static CartItemFetchResponse mapToCartItem(CartItem cartItem, ItemStock itemStock,
-		TimeDealItem timeDealItem) {
+	public static CartItemFetchResponse mapToCartItem(CartItem cartItem, ItemStock itemStock, TimeDealItem timeDealItem) {
 		Item item = cartItem.getItem();
 		int quantity = cartItem.getQuantity();
 		int originalPrice = item.getPrice().intValue();
 
-		int discountedPrice = originalPrice;
-		Integer discountRatio = null;
 		boolean isTimeDeal = item.getItemStatus() == ItemStatus.TIME_DEAL;
 
-		if (timeDealItem != null) {
-			discountedPrice = timeDealItem.getPrice().intValue();
-			discountRatio = timeDealItem.getTimeDeal().getDiscountRatio();
-		}
+		int discountedPrice = isTimeDeal
+			? timeDealItem.getPrice().intValue()
+			: originalPrice;
+
+		Integer discountRatio = isTimeDeal
+			? timeDealItem.getTimeDeal().getDiscountRatio()
+			: null;
 
 		int totalPrice = discountedPrice * quantity;
 		boolean isSoldOut = itemStock.getQuantity() == 0;
+
+		String typeName = item.getTypeBrand().getType().getName();
+		String brandName = item.getTypeBrand().getBrand().getName();
 
 		return new CartItemFetchResponse(
 			cartItem.getId(),
 			item.getId(),
 			item.getName(),
-			item.getTypeBrand().getType().getName(),
-			item.getTypeBrand().getBrand().getName(),
+			typeName,
+			brandName,
 			quantity,
 			item.getImageUrl(),
 			originalPrice,
