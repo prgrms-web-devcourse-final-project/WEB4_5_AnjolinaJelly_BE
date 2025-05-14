09412f30e3b956d3239c558c7e65075a649b95e5
cloudmato <cloudmato00@gmail.com>
2025-05-14 13:33:03 +0900

    Fix: cloned item 저장 직후 외래키 제약 오류 방지를 위한 flush 추가

--- src/main/java/com/jelly/zzirit/domain/item/service/CommandTimeDealService.java
+++ src/main/java/com/jelly/zzirit/domain/item/service/CommandTimeDealService.java
@@ -2,7 +2,6 @@ package com.jelly.zzirit.domain.item.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -37,39 +36,54 @@ public class CommandTimeDealService {
 	@Transactional
 	public TimeDealCreateResponse createTimeDeal(TimeDealCreateRequest request) {
 
+		// 1. 요청 정보로 타임딜을 먼저 저장합니다.(타임딜 아이템 제외)
 		TimeDeal timeDeal = timeDealRepository.save(
 			new TimeDeal(
 				request.title(),
 				TimeDeal.TimeDealStatus.SCHEDULED,
 				request.startTime(),
 				request.endTime(),
-				request.discountRatio()
-			)
-		);
+				request.discountRatio()));
 
-		List<TimeDealCreateResponse.TimeDealCreateItem> responseItems = new ArrayList<>();
-
+		// 2. 요청으로 들어온 items(id, quantity)와 위에서 저장한 타임딜 정보로 타임딜 아이템을 저장합니다.
 		request.items().forEach(item -> {
-			Item originItem = itemRepository.findById(item.itemId())
-				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
 
-			Item clonedItem = itemRepository.save(new Item(
-				originItem.getName(),
-				originItem.getImageUrl(),
-				originItem.getPrice(),
-				ItemStatus.TIME_DEAL,
-				originItem.getTypeBrand()
-			));
+			// 2-1. 타임딜에 등록된 아이템은 기존 아이템(originItem)내용에 Type만 TIME_DEAL인 새로운 아이템으로 새롭게 저장됩니다.
+			Item originItem = itemRepository.findById(item.itemId()).orElseThrow();    // 해당 상품이 없다면? -> 예외처리 필요.
+			Item clonedItemForTimeDeal = itemRepository.save(new Item(
+					originItem.getName(),
+					originItem.getImageUrl(),
+					originItem.getPrice(),
+					ItemStatus.TIME_DEAL,    // 타입만 변경
+					originItem.getTypeBrand()
+				)
+			);
+			itemRepository.flush();
 
-			BigDecimal discountedPrice = clonedItem.getPrice().multiply(
+			// 2-2. 저장된 타임딜과, 타입이 타임딜인 아이템을 이용해 중간 엔티티인 타임딜 아이템을 저장합니다.
+
+			// 타임딜 할인율 적용 가격 계산
+			BigDecimal discountedPrice = clonedItemForTimeDeal.getPrice().multiply(
 				BigDecimal.ONE.subtract(BigDecimal.valueOf(timeDeal.getDiscountRatio()).divide(BigDecimal.valueOf(100)))
 			);
 
-			timeDealItemRepository.save(new TimeDealItem(discountedPrice, timeDeal, clonedItem));
-			itemStockRepository.save(new ItemStock(clonedItem, item.quantity(), item.quantity()));
+			// 타임딜 아이템 저장
+			timeDealItemRepository.save(new TimeDealItem(discountedPrice, timeDeal, clonedItemForTimeDeal));
 
-			responseItems.add(TimeDealCreateResponse.TimeDealCreateItem.from(clonedItem.getId(), item.quantity()));
+			// 2-3. 요청에 포함된 quantity을 이용해 상품 재고를 저장합니다.
+			itemStockRepository.save(new ItemStock(clonedItemForTimeDeal, item.quantity(), item.quantity()));
 		});
+
+		// 응답
+		List<TimeDealCreateResponse.TimeDealCreateItem> responseItems =
+			timeDealItemRepository.findAllByTimeDeal(timeDeal).stream()
+				.map(tdi -> {
+					Long itemId = tdi.getItem().getId();
+					int quantity = itemStockRepository.findByItemId(itemId)
+						.map(ItemStock::getQuantity)
+						.orElse(0);
+					return TimeDealCreateResponse.TimeDealCreateItem.from(itemId, quantity);
+				}).toList();
 
 		return TimeDealCreateResponse.from(
 			timeDeal.getId(),
