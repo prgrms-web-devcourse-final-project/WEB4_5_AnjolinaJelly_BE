2c5c6342473ad725f23ae9bb492758d735654286
jisukimtech <jisukim.tech@gmail.com>
2025-05-16 11:23:41 +0900

    Chore: OrderStatusScheduler에 조건부 실행 반영

--- src/main/java/com/jelly/zzirit/domain/order/scheduler/OrderStatusScheduler.java
+++ src/main/java/com/jelly/zzirit/domain/order/scheduler/OrderStatusScheduler.java
@@ -1,6 +1,7 @@
 package com.jelly.zzirit.domain.order.scheduler;
 
 import com.jelly.zzirit.domain.order.service.order.CommandOrderService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class OrderStatusScheduler {
 
+    @Value("${scheduler.activate-order-scheduler}")
+    private boolean isActivated;
+
     private final CommandOrderService commandOrderService;
 
     @Scheduled(cron = "0 */10 * * * *") // 매 10분마다 실행
     public void updatePaidOrdersToCompleted() {
-        int updatedOrderCount = commandOrderService.completeExpiredOrders();
-        log.info("총 {}개의 주문을 COMPLETED 상태로 변경했습니다.", updatedOrderCount);
+        if (isActivated) {
+            int updatedOrderCount = commandOrderService.completeExpiredOrders();
+            log.info("총 {}개의 주문을 COMPLETED 상태로 변경했습니다.", updatedOrderCount);
+        }
     }
 
 }
