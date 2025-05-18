6c0ad5b8888b7993d3054ba852d2605ab8c05c32
cloudmato <109732067+cloudmato@users.noreply.github.com>
2025-05-18 23:30:47 +0900

    Test: 타임딜 도메인 테스트 추가 (#201)
    * Test: QueryTimeDealService 테스트 추가
    
    * Test: TimeDealSchedulerServiceTest 테스트 추가
    
    * Test: 타임딜 테스트용 ItemFixture추가
    
    * Test: 진행중인 타임딜 테스트 추가
    
    * Test: CommandTimeDealServiceTest 추가

--- src/test/java/com/jelly/zzirit/domain/item/domain/fixture/ItemFixture.java
+++ src/test/java/com/jelly/zzirit/domain/item/domain/fixture/ItemFixture.java
@@ -2,8 +2,10 @@ package com.jelly.zzirit.domain.item.domain.fixture;
 
 import java.math.BigDecimal;
 
+import com.jelly.zzirit.domain.item.entity.Brand;
 import com.jelly.zzirit.domain.item.entity.Item;
 import com.jelly.zzirit.domain.item.entity.ItemStatus;
+import com.jelly.zzirit.domain.item.entity.Type;
 import com.jelly.zzirit.domain.item.entity.TypeBrand;
 
 public class ItemFixture {
@@ -15,6 +17,17 @@ public class ItemFixture {
 			.itemStatus(ItemStatus.NONE)
 			.imageUrl("")
 			.typeBrand(typeBrand)
+			.build();
+	}
+
+	public static Item 삼성_노트북(ItemStatus itemStatus) {
+		return Item.builder()
+			.id(1L)
+			.name("삼성 15인치 노트북")
+			.price(BigDecimal.valueOf(1000000))
+			.itemStatus(itemStatus)
+			.imageUrl("")
+			.typeBrand(new TypeBrand(new Type("노트북"), new Brand("삼성")))
 			.build();
 	}
 
