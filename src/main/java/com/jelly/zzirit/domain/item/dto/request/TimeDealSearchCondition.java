738402ff479758f60cb6c29e1d881a4f090c2b3f
cloudmato <cloudmato00@gmail.com>
2025-05-20 10:41:06 +0900

    Refactor: 타임딜 조회 로직 수정

--- src/main/java/com/jelly/zzirit/domain/item/dto/request/TimeDealSearchCondition.java
+++ src/main/java/com/jelly/zzirit/domain/item/dto/request/TimeDealSearchCondition.java
@@ -1,16 +1,16 @@
 package com.jelly.zzirit.domain.item.dto.request;
 
-import com.jelly.zzirit.domain.item.entity.timedeal.TimeDeal;
+import com.jelly.zzirit.domain.item.entity.timedeal.TimeDeal.TimeDealStatus;
 
 public record TimeDealSearchCondition(
 	String timeDealName,
 	Long timeDealId,
 	String timeDealItemName,
 	Long timeDealItemId,
-	TimeDeal.TimeDealStatus status
+	TimeDealStatus status
 ) {
 	public static TimeDealSearchCondition from(String timeDealName, Long timeDealId, String timeDealItemName,
-		Long timeDealItemId, TimeDeal.TimeDealStatus status) {
+		Long timeDealItemId, TimeDealStatus status) {
 		return new TimeDealSearchCondition(timeDealName, timeDealId, timeDealItemName, timeDealItemId, status);
 	}
 }
