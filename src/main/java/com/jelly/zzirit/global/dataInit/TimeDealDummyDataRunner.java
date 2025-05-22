16bd9bb8f6b3e9ebabf33a29ca63d7068c692df7
huipadyam <kces0822@gmail.com>
2025-05-22 18:41:32 +0900

    Feat: 타임딜 데이터 삽입은 dev, prod 프로필에서만 자동으로 됨

--- src/main/java/com/jelly/zzirit/global/dataInit/TimeDealDummyDataRunner.java
+++ src/main/java/com/jelly/zzirit/global/dataInit/TimeDealDummyDataRunner.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("!test")
+@Profile({"dev", "prod"})
 @Component
 @RequiredArgsConstructor
 public class TimeDealDummyDataRunner implements CommandLineRunner {
