0061b9984bf3c7c632f5784dd0e63b1e0a75cb4f
huipadyam <kces0822@gmail.com>
2025-05-25 20:12:45 +0900

    Chore: 더 이상 안 쓰는 코드 주석 처리

--- src/main/java/com/jelly/zzirit/domain/item/scheduler/DelayQueueProducer.java
+++ src/main/java/com/jelly/zzirit/domain/item/scheduler/DelayQueueProducer.java
@@ -1,31 +1,31 @@
-package com.jelly.zzirit.domain.item.scheduler;
-
-import lombok.AllArgsConstructor;
-
-import java.time.Instant;
-import java.util.Date;
-import java.util.UUID;
-import java.util.concurrent.BlockingQueue;
-
-@AllArgsConstructor
-public class DelayQueueProducer implements Runnable {
-    private final BlockingQueue<DelayedObject> queue;
-    private final Integer numberOfElements;
-    private final int delay;
-    private final int interval;
-
-    @Override
-    public void run() {
-        for(int i = 0; i < numberOfElements; i++){
-            DelayedObject object = new DelayedObject(UUID.randomUUID().toString(), delay);
-            System.out.println("(" + Date.from(Instant.now()) + ") Put object : " + object.getData());
-
-            try{
-                queue.put(object); // 큐가 가득찬 경우 기다렸다가 넣음
-                Thread.sleep(interval); // 다음 객체 넣기 전까지 interval만큼 대기
-            } catch(InterruptedException ie){
-                ie.printStackTrace();
-            }
-        }
-    }
-}
+//package com.jelly.zzirit.domain.item.scheduler;
+//
+//import lombok.AllArgsConstructor;
+//
+//import java.time.Instant;
+//import java.util.Date;
+//import java.util.UUID;
+//import java.util.concurrent.BlockingQueue;
+//
+//@AllArgsConstructor
+//public class DelayQueueProducer implements Runnable {
+//    private final BlockingQueue<DelayedObject> queue;
+//    private final Integer numberOfElements;
+//    private final int delay;
+//    private final int interval;
+//
+//    @Override
+//    public void run() {
+//        for(int i = 0; i < numberOfElements; i++){
+//            DelayedObject object = new DelayedObject(UUID.randomUUID().toString(), delay);
+//            System.out.println("(" + Date.from(Instant.now()) + ") Put object : " + object.getData());
+//
+//            try{
+//                queue.put(object); // 큐가 가득찬 경우 기다렸다가 넣음
+//                Thread.sleep(interval); // 다음 객체 넣기 전까지 interval만큼 대기
+//            } catch(InterruptedException ie){
+//                ie.printStackTrace();
+//            }
+//        }
+//    }
+//}
