c098c2de3de3e39ea8d1a998136522bf6a138d4a
cloudmato <cloudmato00@gmail.com>
2025-05-26 21:12:53 +0900

    Fix: 카카오 OAuth 401 문제 해결

--- src/main/java/com/jelly/zzirit/global/dataInit/TimeDealDummyDataRunner.java
+++ src/main/java/com/jelly/zzirit/global/dataInit/TimeDealDummyDataRunner.java
@@ -1,19 +1,20 @@
 package com.jelly.zzirit.global.dataInit;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile({"dev", "prod"})
+import lombok.RequiredArgsConstructor;
+
+@Profile({"prod"})
 @Component
 @RequiredArgsConstructor
 public class TimeDealDummyDataRunner implements CommandLineRunner {
 
-    private final TimeDealDummyDataGenerator generator;
+	private final TimeDealDummyDataGenerator generator;
 
-    @Override
-    public void run(String... args) throws Exception {
-        generator.generateInitialData();
-    }
+	@Override
+	public void run(String... args) throws Exception {
+		generator.generateInitialData();
+	}
 }
